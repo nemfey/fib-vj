b0e534b68a0f3e6e575b1f9981318e0adbec8df0
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-03-27 23:10:54 +0200

    help screen done

--- 02-Bubble/02-Bubble/Menu.cpp
+++ 02-Bubble/02-Bubble/Menu.cpp
@@ -32,7 +32,7 @@ void Menu::init(ShaderProgram& shaderProgram)
 	//spritesheet.loadFromFile("images/menu_img.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	//spritesheet.setMagFilter(GL_NEAREST);
 	//sprite = Sprite::createSprite(glm::ivec2(720, 405), glm::vec2(1, 1), &spritesheet, &shaderProgram);
-	mainSpritesheet.loadFromFile("images/menu.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	mainSpritesheet.loadFromFile("images/menu_title_new.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	mainSpritesheet.setMagFilter(GL_NEAREST);
 	mainSprite = Sprite::createSprite(glm::ivec2(720, 405), glm::vec2(0.5f, 0.5f), &mainSpritesheet, &shaderProgram);
 	
@@ -47,9 +47,21 @@ void Menu::init(ShaderProgram& shaderProgram)
 	mainSprite->changeAnimation(0);
 	mainSprite->setPosition(glm::vec2(0.f, 0.f));
 	
-	helpSpriteSheet.loadFromFile("images/help1.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	helpSpriteSheet.loadFromFile("images/help_menu.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	helpSpriteSheet.setMagFilter(GL_NEAREST);
-	helpSprite = Sprite::createSprite(glm::ivec2(720, 405), glm::vec2(1.f, 1.f), &helpSpriteSheet, &shaderProgram);
+	helpSprite = Sprite::createSprite(glm::ivec2(720, 405), glm::vec2(0.25f, 0.5f), &helpSpriteSheet, &shaderProgram);
+	helpSprite->setNumberAnimations(1);
+	helpSprite->setAnimationSpeed(0, 2);
+	helpSprite->addKeyframe(0, glm::vec2(0.f, 0.f));
+	helpSprite->addKeyframe(0, glm::vec2(0.25f, 0.f));
+	helpSprite->addKeyframe(0, glm::vec2(0.5f, 0.f));
+	helpSprite->addKeyframe(0, glm::vec2(0.75f, 0.f));
+	helpSprite->addKeyframe(0, glm::vec2(0.f, 0.5f));
+	helpSprite->addKeyframe(0, glm::vec2(0.25f, 0.5f));
+	helpSprite->addKeyframe(0, glm::vec2(0.5f, 0.5f));
+	helpSprite->addKeyframe(0, glm::vec2(0.75f, 0.5f));
+
+	helpSprite->changeAnimation(0);
 	helpSprite->setPosition(glm::vec2(0.f, 0.f));
 
 	creditsSpriteSheet.loadFromFile("images/credits.png", TEXTURE_PIXEL_FORMAT_RGBA);
