7c3a6bac031dfa847314cfe974831275bcad3f33
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-04-18 13:10:23 +0200

    unity structure updated

---
+++ game3D/Runner/Assets/Scripts/CreateLevel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLevel : MonoBehaviour
+{
+    public GameObject floorPrefab, turnRightPrefab, turnLeftPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
