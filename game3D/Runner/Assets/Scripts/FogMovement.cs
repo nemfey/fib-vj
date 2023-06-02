0cb1d4652221779fbf082d47129a3b49d2bba46f
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-06-02 16:45:21 +0200

    fog fixed

--- game3D/Runner/Assets/Scripts/FogMovement.cs
+++ game3D/Runner/Assets/Scripts/FogMovement.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class FogMovement : MonoBehaviour
 {
-    public Vector3 playerCoords;
+    Vector3 playerCoords;
     GameObject player;
-    float minY;
+    float offsetY, minY;
+
+    public GameObject fogPrefab;
+    GameObject farFog, nearFog;
 
     // Start is called before the first frame update
     void Start()
     {
+        offsetY = 15f;
         player = GameObject.Find("Player");
-        minY = player.GetComponent<Transform>().position.y - 20;
-        //playerCoords = GameObject.Find("Player").GetComponent<Transform>().position;
+        playerCoords = GameObject.Find("Player").GetComponent<Transform>().position;
+        minY = playerCoords.y - offsetY;
+
+        farFog = (GameObject)Instantiate(fogPrefab);
+        farFog.transform.Translate(playerCoords.x - 200f, minY, playerCoords.z - 200f);
+        farFog.transform.parent = transform;
+
+        nearFog = (GameObject)Instantiate(fogPrefab);
+        nearFog.transform.Translate(playerCoords.x, minY, playerCoords.z);
+        nearFog.transform.parent = transform;
     }
 
     // Update is called once per frame
@@ -21,9 +33,20 @@ public class FogMovement : MonoBehaviour
     {
         playerCoords = player.GetComponent<Transform>().position;
 
-        if (playerCoords.y - 20 < minY)
-            minY = playerCoords.y - 20;
+        if (playerCoords.y - offsetY < minY)
+            minY = playerCoords.y - offsetY;
 
-        this.transform.position = new Vector3(playerCoords.x, minY, playerCoords.z);
+        float distance = Vector3.Distance(playerCoords, farFog.transform.position);
+        if (distance > 400f)
+        {
+            Destroy(farFog);
+            farFog = nearFog;
+
+            nearFog = (GameObject)Instantiate(fogPrefab);
+            nearFog.transform.Translate(playerCoords.x + 100f, minY, playerCoords.z + 100f);
+            nearFog.transform.parent = transform;
+        }
+
+        //this.transform.position = new Vector3(playerCoords.x, minY, playerCoords.z);
     }
 }
