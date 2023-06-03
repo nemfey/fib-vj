c64bd5458060e211ec263a424b14da43f5e33c46
Joan Sales <joan.sales.de.marcos@estudiantat.upc.edu>
2023-06-03 04:15:50 +0200

    No entiendo nada

--- game3D/Runner/Assets/Scripts/DeathParticleController.cs
+++ game3D/Runner/Assets/Scripts/DeathParticleController.cs
@@ -6,28 +6,19 @@ public class DeathParticleController : MonoBehaviour
 {
     private ParticleSystem particleSystem;
 
-    public Transform targetObject;
-
     private void Awake()
     {
         particleSystem = GetComponent<ParticleSystem>();
-        //particleSystem.Stop(); // Asegurarse de que el sistema de partículas esté desactivado al principio
-    }
-
-    private void Update()
-    {
-        if (targetObject != null)
-        {
-            // Calcula la dirección hacia el objeto objetivo
-            Vector3 direction = targetObject.position - transform.position;
-
-            // Mueve el objeto seguidor en dirección al objeto objetivo
-            transform.Translate(direction.normalized * Time.deltaTime);
-        }
+        particleSystem.Stop(); // Asegurarse de que el sistema de partículas esté desactivado al principio
     }
 
     public void ActivateParticleSystem()
     {
         particleSystem.Play();
+    }
+
+    public void StopParticleSystem()
+    {
+        particleSystem.Stop();
     }
 }
